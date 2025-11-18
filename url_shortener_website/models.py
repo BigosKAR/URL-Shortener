99b8df42074a081965bdedba1c2964698efc668f
Oskar K <oskarkloczko13@gmail.com>
2025-11-18 21:09:52 +0100

    removes comment to execute workflow

--- url_shortener_website/models.py
+++ url_shortener_website/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class UrlMapping(models.Model):
     id = models.BigAutoField(primary_key=True) # Support massive amounts of entry IDs (for example: 100 million new URLs every day)
